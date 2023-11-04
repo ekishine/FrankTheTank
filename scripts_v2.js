3f39c4012aad660b01e3a9ef4872c41d140ecd1b
ekishine <108537087+ekishine@users.noreply.github.com>
2023-11-04 17:03:34 +0100

    Update scripts_v2.js

--- scripts_v2.js
+++ scripts_v2.js
@@ -1,15 +1,11 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Target the 'welcome-message' div and set its inner HTML
     var welcomeDiv = document.getElementById('welcome-message');
     if (welcomeDiv) {
-        // You've already set the inner HTML to include an h1 tag in the welcomeDiv
-        welcomeDiv.innerHTML = '<h1>Welcome, Master Jedi! Your quest beckons...</h1>';
-        
-        // Now target the h1 for the CircleType.js effect
-        // Since innerHTML has just been set, it is the first child of welcomeDiv
-        var h1 = welcomeDiv.querySelector('h1'); // Get the h1 element
-
-        // Apply the CircleType.js effect to the h1 element
-        new CircleType(h1).radius(384); // Adjust the radius to fit your design
+        // Assuming the welcomeDiv already contains <h1> with the text from your HTML
+        var h1 = welcomeDiv.querySelector('h1');
+        if(h1){
+            new CircleType(h1).radius(384); // Apply the CircleType effect to the h1 element
+        }
     }
 });
+
