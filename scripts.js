e960a2d31bb57538fad53d4b1c5c79aa151f5711
ekishine <108537087+ekishine@users.noreply.github.com>
2023-11-04 12:56:35 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,5 @@
-// Function to display a welcome message using alert
-function showWelcomeMessage() {
-  alert("Welcome, Master Jedi! Your quest beckons.");
-}
-
 // Event listener for when the DOM content has loaded
 document.addEventListener('DOMContentLoaded', (event) => {
-  // This will run when the page is fully loaded, including images
-  showWelcomeMessage(); // Show the popup alert message
-
   // If you have a div with an id of 'welcome-message' in your HTML,
   // this code will insert text into that div.
   var welcomeDiv = document.getElementById('welcome-message');
